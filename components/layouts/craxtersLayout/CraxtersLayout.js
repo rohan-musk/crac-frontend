590c7b26872303f98a1b41f8a4af0fa0e96e791c
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2023-08-04 16:21:38 +0530

    RM|Fixed the craxters bug

--- components/layouts/craxtersLayout/CraxtersLayout.js
+++ components/layouts/craxtersLayout/CraxtersLayout.js
@@ -15,33 +15,34 @@ import {
 import axios from 'axios';
 
 const CraxtersLayout = () => {
-  const [allUsers, setAllUsers] = useState({
+  const [allUsers, setAllUsers] = useState({});
+
+  let users = {
     head: [],
     member: [],
     previousMember: [],
-  });
+  };
+
   useEffect(() => {
     axios.get('http://localhost:3001/user/getUsers').then((response) => {
       response.data.allUsers.forEach((element) => {
         if (element.crac_member == true) {
           if (element.role === '') {
-            setAllUsers({ ...allUsers, member: allUsers.member.push(element) });
+            users = { ...users, member: [...users.member, element] };
           } else if (element.role === 'ex') {
-            setAllUsers({
-              ...allUsers,
-              previousMember: allUsers.previousMember.push(element),
-            });
+            users = {
+              ...users,
+              previousMember: [...users.previousMember, element],
+            };
           } else {
-            setAllUsers({ ...allUsers, head: allUsers.head.push(element) });
+            users = { ...users, head: [...users.head, element] };
           }
+          console.log(users);
         }
       });
+      setAllUsers(users);
     });
   }, []);
-
-  useEffect(() => {
-    console.log(allUsers);
-  }, [allUsers]);
 
   return (
     <CraxtersLayoutContainer>
@@ -51,25 +52,27 @@ const CraxtersLayout = () => {
       <CraxtersBody>
         <MemberHeading>Heads</MemberHeading>
         <MemberLayout>
-          {allUsers.head.map((e) => {
-            return (
-              <Member href={'/craxters/' + e.id + '/work'} key={e.id}>
-                <MemberImage>
-                  <Image
-                    loader={() => e.picture}
-                    src={e.picture}
-                    width={150}
-                    height={150}
-                    alt='Picture of the author'
-                    layout='fit'
-                    objectFit='cover'
-                  />
-                </MemberImage>
-                <MemberName>{e.name}</MemberName>
-                <MemberDesignation>{e.role}</MemberDesignation>
-              </Member>
-            );
-          })}
+          {Array.isArray(allUsers.head)
+            ? allUsers.head.map((e) => {
+                return (
+                  <Member href={'/craxters/' + e.id + '/work'} key={e.id}>
+                    <MemberImage>
+                      <Image
+                        loader={() => e.picture}
+                        src={e.picture}
+                        width={150}
+                        height={150}
+                        alt='Picture of the author'
+                        layout='fit'
+                        objectFit='cover'
+                      />
+                    </MemberImage>
+                    <MemberName>{e.name}</MemberName>
+                    <MemberDesignation>{e.role}</MemberDesignation>
+                  </Member>
+                );
+              })
+            : null}
         </MemberLayout>
         <MemberHeading>Members</MemberHeading>
         <MemberLayout>
